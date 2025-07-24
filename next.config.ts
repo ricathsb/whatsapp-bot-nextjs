618b35470f5a43bbf5674753565c8a4a4c8ef2a0
ricathsb <ricat1907111@gmail.com>
2025-07-24 14:33:47 +0700

    for nginx

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 // next.config.ts
 
 const nextConfig = {
+  basePath: "/blasting", // ⬅️ Tambahkan ini
+  trailingSlash: true,   // ⬅️ Opsional, supaya semua URL diakhiri "/"
   experimental: {
     turbo: false, // ✅ Nonaktifkan Turbopack, pakai Webpack
   },
