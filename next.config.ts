1ac2572b04177f969c4f49382e3541fa1c1e804e
ricathsb <ricat1907111@gmail.com>
2025-07-24 16:01:52 +0700

    ku entak la ini argkkfawfklawnaklwf

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 // next.config.ts
 
 const nextConfig = {
-  basePath: "/blasting", // ⬅️ Tambahkan ini
-  trailingSlash: true,   // ⬅️ Opsional, supaya semua URL diakhiri "/"
   experimental: {
     turbo: false, // ✅ Nonaktifkan Turbopack, pakai Webpack
   },
